50d8a307cb1a5a48d1ab3c6b1d2320264d6c6cf3
Vikram <ahir345@gmail.com>
2023-09-07 11:04:01 +0530

    Fixed words

--- lib/presentation/home/bloc/bloc/home_bloc.dart
+++ lib/presentation/home/bloc/bloc/home_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:demo_bloc_currency/core/api/rest_client.dart';
+import 'package:demo_bloc_currency/core/utils/common.dart';
 import 'package:demo_bloc_currency/model/currency_data.dart';
 import 'package:equatable/equatable.dart';
 
@@ -16,7 +17,8 @@ class HomeBloc extends Bloc<HomeEvent, HomeState> {
       GetCurrencyDataEvent getCurrencyDataEvent, Emitter emitter) async {
     emitter.call(const HomeLoadingState());
     final result = await restClient.get(
-        path: 'convertcurrency?have=USD&want=INR&amount=100');
+      path: Common.apiPath,
+    );
     Map<String, dynamic> mData = result.data;
     CurrencyData currencyData = CurrencyData.fromJson(mData);
     emitter.call(HomeLoaddedState(currencyData: currencyData));
